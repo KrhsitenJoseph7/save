eec6a08acaac68e5e127afed7841dde4ef9a8c17
@KrhistenJoseph7 <kristhenpino17@gmail.com>
2019-06-05 12:10:14 -0500

    Conctacto
    pagina contacto

--- Enviar.php
+++ Enviar.php
@@ -3,8 +3,8 @@
   $NOMBRE = $_POST["NOMBRE"];
   $CORREO = $_POST["CORREO"];
   $TELEFONO = $_POST["TELEFONO"];
-  $Mensaje = $_POST["mensaje"];
-  $contenido = "Nombre: " . $NOMBRE ."\nCorreo" .$CORREO. "\nTelefono".$TELEFONO. "\nMensaje: . $mensaje";
+  $mensaje = $_POST["mensaje"];
+  $contenido = "Nombre: " . $NOMBRE ."\nCorreo" .$CORREO. "\nTelefono".$TELEFONO. "\nMensaje: " . $mensaje;
   mail($destino,"Contacto",$contenido);
   header("Location:gracias.html");
 ?>
